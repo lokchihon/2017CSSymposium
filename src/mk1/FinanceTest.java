472af0a6f63b37e4924dbcd5c0c69f361a392d55
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-05-24 13:29:28 -0400

    Test

---
+++ src/mk1/FinanceTest.java
@@ -0,0 +1,21 @@
+package mk1;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public class FinanceTest {
+
+	public void main(String[]args) throws IOException{
+		Stock stock = YahooFinance.get("INTC");
+		 
+		BigDecimal price = stock.getQuote().getPrice();
+		BigDecimal change = stock.getQuote().getChangeInPercent();
+		BigDecimal peg = stock.getStats().getPeg();
+		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+		 
+		stock.print();
+	}
+}
