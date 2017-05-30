baea53d063c5c4ba9dcc2609b4b38a9346810dce
Lok Chi <lhon4644@bths.edu>
2017-05-30 13:34:04 -0400

    added more stocks

--- src/webCrawler/finalSearch.java
+++ src/webCrawler/finalSearch.java
@@ -9,13 +9,25 @@ import org.jsoup.select.Elements;
 
 public class finalSearch {
 
-	public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search?q=aapl+news&rlz=1C1CHBF_enUS704US704&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjIjfTG2JXUAhWs64MKHeaYC5MQ_AUICigB&biw=674&bih=762";
+	//AAPL
+	public static final String GOOGLE_SEARCH_URL1 = "https://www.google.com/search?q=aapl+news&rlz=1C1CHBF_enUS704US704&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjIjfTG2JXUAhWs64MKHeaYC5MQ_AUICigB&biw=674&bih=762";
+	//INTC
+	public static final String GOOGLE_SEARCH_URL2 = "https://www.google.com/search?q=intc+news&rlz=1C1CHBF_enUS704US704&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjIjfTG2JXUAhWs64MKHeaYC5MQ_AUICigB&biw=674&bih=762";
+	//DOW
+	//TSLA
+	//MSFT
+	//GE
+	//JPM
+	//AMZN
+	//XOM
+	
+	
 	public static void main(String[] args) throws IOException {
 	System.out.println("APPLE NEWS");
 	String searchTerm = "AAPL";
 	int num = 10;
 		
-	String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+num;
+	String searchURL = GOOGLE_SEARCH_URL1 + "?q="+searchTerm+"&num="+num;
 	//without proper User-Agent, we will get 403 error
 	Document doc = Jsoup.connect(searchURL).userAgent("Chrome").get();
 		
