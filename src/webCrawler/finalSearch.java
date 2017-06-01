0923b1c7c0778fa971364a5e3ea12cfb3ab6ede8
Lok Chi <lhon4644@bths.edu>
2017-06-01 13:35:09 -0400

    not done sorting

--- src/webCrawler/finalSearch.java
+++ src/webCrawler/finalSearch.java
@@ -49,9 +49,80 @@ public class finalSearch {
 		google2[6] = "JPMORGAN";
 		google2[7] = "AMAZON";
 		google2[8] = "EXXON MOBILE";
-		
 	}
 	
+	
+	/**
+	 * Search for one word in phrase. The search is not
+	 *  case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no").
+	 *
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @param startPos
+	 *            the character of the string to begin the
+	 *            search at
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal, int startPos){
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0){
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0){
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length()){
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+	
+	/**
+	if (findKeyword(statement, "swim") >= 0
+			|| findKeyword(statement, "run") >= 0
+			|| findKeyword(statement, "football") >= 0
+			|| findKeyword(statement, "soccer") >= 0
+			|| findKeyword(statement, "baseball") >= 0
+			|| findKeyword(statement, "badminton") >= 0
+			|| findKeyword(statement, "tennis") >= 0
+			|| findKeyword(statement, "basketball") >= 0)
+		**/
+
 	public static void main(String[] args) throws IOException {
 		finalSearch.setter();
 		int counter = 0;
@@ -72,11 +143,9 @@ public class finalSearch {
 			for (Element result : results) {
 				String linkHref = result.attr("href");
 				String linkText = result.text();
-				System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
+				System.out.println("Text:: " + linkText + ", URL:: " + linkHref.substring(6, linkHref.indexOf("&")));
 			}
 			System.out.println();
-
-			
 		}
 		
 	}
