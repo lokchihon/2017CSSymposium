723af5f78ac37848ee907b4a7cd7721ee6486896
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-06-05 12:58:20 -0400

    Finance

--- src/mk1/FinanceTest.java
+++ src/mk1/FinanceTest.java
@@ -2,20 +2,37 @@ package mk1;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import yahoofinance.histquotes.HistQuotesRequest;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.quotes.fx.FxQuote;
+import yahoofinance.quotes.fx.FxQuotesRequest;
+import yahoofinance.quotes.stock.StockQuotesData;
+import yahoofinance.quotes.stock.StockQuotesRequest;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 public class FinanceTest {
 
-	public void main(String[]args) throws IOException{
-		Stock stock = YahooFinance.get("DOW");
-		 
+	public static void main(String[]args) throws IOException{
+		Stock stock = YahooFinance.get("INTC");
+		 /*
 		BigDecimal price = stock.getQuote().getPrice();
 		BigDecimal change = stock.getQuote().getChangeInPercent();
 		BigDecimal peg = stock.getStats().getPeg();
 		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
-		 
-		stock.print();
+		 */
+		
+		String[] symbols = new String[] {"INTC", "BABA", "TSLA", "AIR.PA", "YHOO"};
+		Map<String, Stock> stocks = YahooFinance.get(symbols); // single request
+		Stock intel = stocks.get("INTC");
+		Stock airbus = stocks.get("AIR.PA");
+		intel.print();
+		airbus.print();
 	}
 }
