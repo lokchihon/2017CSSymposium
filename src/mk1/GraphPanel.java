697ee4c551aa0b788c42f2174d7a41630dbcdf66
Darren Kong <dkong1228@bths.edu>
2017-06-07 13:31:14 -0400

    fk

--- src/mk1/GraphPanel.java
+++ src/mk1/GraphPanel.java
@@ -10,14 +10,20 @@ import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import webCrawler.finalSearch;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 public class GraphPanel extends JPanel {
 	
@@ -188,6 +194,32 @@ public class GraphPanel extends JPanel {
     	String i = sc.nextLine();
     	finalSearch.setter();
     	finalSearch.whichArticle(i);
+    	
+		ArrayList <Double> tenminprice = new ArrayList<Double>();
+		Stock stock = YahooFinance.get("INTC");
+		 /*
+		BigDecimal price = stock.getQuote().getPrice();
+		BigDecimal change = stock.getQuote().getChangeInPercent();
+		BigDecimal peg = stock.getStats().getPeg();
+		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+		 */
+		
+		for(int x = 0; x < 10; x++){
+			String[] symbols = new String[] {"INTC", "BABA", "TSLA", "AIR.PA", "YHOO"};
+			Map<String, Stock> stocks = YahooFinance.get(symbols); // single request
+			Stock intel = stocks.get("INTC");
+			//Stock airbus = stocks.get("AIR.PA");
+			BigDecimal price = intel.getQuote().getPrice();
+			System.out.println(price.doubleValue());
+			tenminprice.add(price.doubleValue());
+			try {
+				TimeUnit.MINUTES.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.print(Arrays.toString(tenminprice.toArray()));
     	
     	double[] arry = {2.5, 3.5,10.3, 5.0, 7.1};
     	scoring = arry;
